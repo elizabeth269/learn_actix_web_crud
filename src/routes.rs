931c251c4f8527792003fcf3f33752a188783a70
Elizbeth <elizabethadebayo269@gmail.com>
2024-07-06 22:48:11 +0100

    added code to my routes.rs

--- src/routes.rs
+++ src/routes.rs
@@ -1,1 +1,11 @@
+use actix_web::web;
 
+use crate::handlers::{create_item, delete_item, get_item, get_items, update_item};
+
+pub fn configure(cfg: &mut web::ServiceConfig) {
+    cfg.service(create_item)
+        .service(get_items)
+        .service(get_item)
+        .service(update_item)
+        .service(delete_item);
+}
