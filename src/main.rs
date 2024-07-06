d2c53707aaccc7a1e4562b9b011df970925aad28
Elizbeth <elizabethadebayo269@gmail.com>
2024-07-06 22:50:56 +0100

    added my main.rs code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+mod handlers;
+mod models;
+mod routes;
+
+use actix_web::{web, App, HttpServer};
+use handlers::AppState;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let data = web::Data::new(AppState {
+        items: std::sync::Mutex::new(Vec::new()),
+    });
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(data.clone())
+            .configure(routes::configure)
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
